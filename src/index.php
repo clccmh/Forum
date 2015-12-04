dba7427a5b7d3a4fad0b4f6fc738c7e359a56be7
Carter Hay <chay@uno.edu>
2015-12-04 11:39:18 -0600

    Made a basic landing page

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+  <title>Carter's Forum</title>
+  <style>
+    body {
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <h1> Welcome to Carter's Forum</h1>
+  <button>Login</button>
+  <button>Register</button>
+</body>
+
+
+</html>
